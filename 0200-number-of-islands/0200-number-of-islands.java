e486991d4e35de07a23d1110f9ad93b7390891d6
Shikhar <shikharsingh22803@gmail.com>
2025-07-23 02:30:15 +0530

    Time: 5 ms (32.81%), Space: 52.5 MB (14.11%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,36 +1,51 @@
+import java.util.*;
+
 class Solution {
     public int numIslands(char[][] grid) {
-
-        int count = 0;
         int n = grid.length;
         int m = grid[0].length;
+        int count = 0;
 
-        for(int i=0;i<n;i++)
-        {
-            for(int j=0;j<m;j++)
-            {
-                if(grid[i][j]=='1')
-                {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (grid[i][j] == '1') {
                     count++;
-                    dfs(grid,i,j);
+                    bfs(grid, i, j);
                 }
             }
         }
+
         return count;
     }
 
-    public void dfs(char[][]grid, int i, int j)
-    {
-        if(i<0 || i>=grid.length || j<0 || j>=grid[i].length || grid[i][j]=='0')
-        {
-            return;
+    private void bfs(char[][] grid, int i, int j) {
+        int n = grid.length;
+        int m = grid[0].length;
+
+        Queue<int[]> queue = new LinkedList<>();
+        queue.offer(new int[]{i, j});
+        grid[i][j] = '0'; // mark as visited
+
+        int[][] directions = {{1,0}, {-1,0}, {0,1}, {0,-1}}; // down, up, right, left
+
+        while (!queue.isEmpty()) {
+            int[] cell = queue.poll();
+            int row = cell[0];
+            int col = cell[1];
+
+            for (int[] dir : directions) {
+                int newRow = row + dir[0];
+                int newCol = col + dir[1];
+
+                // valid and unvisited land
+                if (newRow >= 0 && newRow < n &&
+                    newCol >= 0 && newCol < m &&
+                    grid[newRow][newCol] == '1') {
+                    
+                    queue.offer(new int[]{newRow, newCol});
+                    grid[newRow][newCol] = '0'; // mark visited
+                }
+            }
         }
-
-        grid[i][j]='0';
-
-        dfs(grid,i+1,j);
-        dfs(grid,i-1,j);
-        dfs(grid,i,j+1);
-        dfs(grid,i,j-1);
     }
 }
