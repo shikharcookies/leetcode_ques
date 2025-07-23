2cea77b1713e0ace4b4486cb24a023463a15383f
Shikhar <shikharsingh22803@gmail.com>
2025-07-23 22:13:32 +0530

    Time: 6 ms (44.38%), Space: 46.4 MB (20.29%) - LeetHub

---
+++ 0210-course-schedule-ii/0210-course-schedule-ii.java
@@ -0,0 +1,63 @@
+class Solution {
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
+          // Form a graph
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>(); 
+        for(int i=0;i<numCourses;i++)
+        {
+            adj.add(new ArrayList<>());
+        }
+
+        int m = prerequisites.length;
+        for(int i=0;i<m;i++)
+        {
+            adj.get(prerequisites[i][1]).add(prerequisites[i][0]);
+        }
+
+
+        int indegree[] = new int[numCourses];
+        for(int i=0;i<numCourses;i++)
+        {
+            for(int it: adj.get(i))
+            {
+                indegree[it]++;
+            }
+        }
+
+        Queue<Integer> q = new LinkedList<>();
+        for(int i=0;i<numCourses;i++)
+        {
+            if(indegree[i]==0)
+            {
+                q.add(i);
+            }
+        }
+
+        List<Integer> topo = new ArrayList<Integer>();
+
+        while(!q.isEmpty())
+        {
+            int node = q.peek();
+            q.remove();
+
+            topo.add(node);
+
+             // node is in your topo sort
+            // so please remove it from the indegree
+            for(int it: adj.get(node))
+            {
+                indegree[it]--;
+                if(indegree[it]==0) q.add(it);
+            }
+        }
+        if(topo.size()!=numCourses){
+            return new int[0];
+        }
+
+        int[] result = new int[numCourses];
+        for(int i=0;i<numCourses;i++)
+        {
+            result[i]=topo.get(i);
+        }
+        return result;
+    }
+}
