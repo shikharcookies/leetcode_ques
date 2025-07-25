f236119bec2da330c9a8af916082802cdb631c30
Shikhar <shikharsingh22803@gmail.com>
2025-07-25 21:28:01 +0530

    Time: 1 ms (68.88%), Space: 42.9 MB (93.86%) - LeetHub

--- 0684-redundant-connection/0684-redundant-connection.java
+++ 0684-redundant-connection/0684-redundant-connection.java
@@ -1,51 +1,49 @@
-public class Solution {
-    private boolean[] visit;
-    private List<List<Integer>> adj;
-    private Set<Integer> cycle;
-    private int cycleStart;
+class DisjointSet {
+    int[] parent, rank;
 
-    public int[] findRedundantConnection(int[][] edges) {
-        int n = edges.length;
-        adj = new ArrayList<>();
-        for (int i = 0; i <= n; i++) 
-            adj.add(new ArrayList<>());
-
-        for (int[] edge : edges) {
-            int u = edge[0], v = edge[1];
-            adj.get(u).add(v);
-            adj.get(v).add(u);
+    public DisjointSet(int n) {
+        parent = new int[n + 1];
+        rank = new int[n + 1];
+        for (int i = 0; i <= n; i++) {
+            parent[i] = i;
+            rank[i] = 0;
         }
-
-        visit = new boolean[n + 1];
-        cycle = new HashSet<>();
-        cycleStart = -1;
-        dfs(1, -1);
-
-        for (int i = edges.length - 1; i >= 0; i--) {
-            int u = edges[i][0], v = edges[i][1];
-            if (cycle.contains(u) && cycle.contains(v)) {
-                return new int[]{u, v};
-            }
-        }
-        return new int[0];
     }
 
-    private boolean dfs(int node, int par) {
-        if (visit[node]) {
-            cycleStart = node;
-            return true;
+    public int findUPar(int node) {
+        if (node == parent[node])
+            return node;
+        return parent[node] = findUPar(parent[node]);
+    }
+
+    public void unionByRank(int u, int v) {
+        int pu = findUPar(u);
+        int pv = findUPar(v);
+        if (pu == pv) return;
+
+        if (rank[pu] < rank[pv]) {
+            parent[pu] = pv;
+        } else if (rank[pu] > rank[pv]) {
+            parent[pv] = pu;
+        } else {
+            parent[pv] = pu;
+            rank[pu]++;
         }
-        visit[node] = true;
-        for (int nei : adj.get(node)) {
-            if (nei == par) continue;
-            if (dfs(nei, node)) {
-                if (cycleStart != -1) cycle.add(node);
-                if (node == cycleStart) {
-                    cycleStart = -1;
-                }
-                return true;
+    }
+}
+
+class Solution {
+    public int[] findRedundantConnection(int[][] edges) {
+        int n = edges.length;
+        DisjointSet ds = new DisjointSet(n);
+        for (int[] edge : edges) {
+            int u = edge[0], v = edge[1];
+            if (ds.findUPar(u) == ds.findUPar(v)) {
+                return edge;
+            } else {
+                ds.unionByRank(u, v);
             }
         }
-        return false;
+        return new int[]{-1, -1};
     }
 }
