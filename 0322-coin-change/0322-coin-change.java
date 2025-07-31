53a4b818f033fec6ee4018d133142532ad7c169e
Shikhar <shikharsingh22803@gmail.com>
2025-08-01 04:07:11 +0530

    Time: 15 ms (76.49%), Space: 45 MB (34.21%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,30 +1,34 @@
-class Solution {
+public class Solution {
     public int coinChange(int[] coins, int amount) {
         int n = coins.length;
 
-        int[] prev = new int[amount+1];
+        int[] prev = new int[amount + 1];
+        int[] cur = new int[amount + 1];
 
-        for(int i=0;i<n;i++)
-        {
-            int[] curr = new int[amount+1];
-
-            for(int k=1;k<=amount;k++)
-            {
-                int use = (int) 1e9, notUse = (int) 1e9;
-
-                if(coins[i]<=k)
-                {
-                    use = 1 + curr[k-coins[i]];
-                }
-                if(i>0)
-                {
-                    notUse = prev[k];
-                }
-
-                curr[k]= Math.min(notUse,use);
-            }
-            prev = curr;
+        // Base case: using only the first coin
+        for (int i = 0; i <= amount; i++) {
+            if (i % coins[0] == 0)
+                prev[i] = i / coins[0];
+            else
+                prev[i] = (int) 1e9;
         }
-        return prev[amount] >= (int) 1e9 ? -1 : prev[amount];
+
+        // Tabulation for remaining coins
+        for (int ind = 1; ind < n; ind++) {
+            for (int target = 0; target <= amount; target++) {
+                int notTake = prev[target];
+                int take = (int) 1e9;
+
+                if (coins[ind] <= target) {
+                    take = 1 + cur[target - coins[ind]];
+                }
+
+                cur[target] = Math.min(take, notTake);
+            }
+            prev = cur.clone(); // update for next iteration
+        }
+
+        int ans = prev[amount];
+        return ans >= (int) 1e9 ? -1 : ans;
     }
 }
