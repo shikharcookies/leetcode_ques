aea9d6c6f1b161a31d5122f9e5486de941789af1
Shikhar <shikharsingh22803@gmail.com>
2025-07-31 17:56:45 +0530

    Time: 19 ms (86.43%), Space: 50.9 MB (67.6%) - LeetHub

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.java
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.java
@@ -3,21 +3,18 @@ class Solution {
         int n = text1.length();
         int m = text2.length();
 
-        int [][]dp = new int[n][m];
-         for (int[] row : dp) {
-            Arrays.fill(row, -1);  // Initialize with -1
+        int[][] dp = new int[n + 1][m + 1]; // +1 for shifting
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (text1.charAt(i - 1) == text2.charAt(j - 1)) {
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
         }
 
-        return fn(n-1,m-1,text1,text2,dp);
-    }
-
-    public int fn(int i,int j,String text1,String text2, int[][]dp)
-    {
-        if(i<0 || j<0) return 0;
-        if(dp[i][j]!= -1) return dp[i][j];
-
-        if(text1.charAt(i)==text2.charAt(j)) return dp[i][j]= 1 + fn(i-1,j-1,text1,text2,dp);
-
-        return dp[i][j]= 0 + Math.max(fn(i,j-1,text1,text2,dp),fn(i-1,j,text1,text2,dp));
+        return dp[n][m];
     }
 }
