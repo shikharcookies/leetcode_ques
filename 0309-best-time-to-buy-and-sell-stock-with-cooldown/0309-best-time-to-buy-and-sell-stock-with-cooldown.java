4e62df7011e3d7aa84780f5af30775b13f7344f9
Shikhar <shikharsingh22803@gmail.com>
2025-08-01 18:18:12 +0530

    Time: 0 ms (100%), Space: 41.7 MB (81.73%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.java
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.java
@@ -1,7 +1,9 @@
 class Solution {
     public int maxProfit(int[] prices) {
          int n = prices.length;
-        int dp[][] = new int[n + 2][2];
+        int[] cur = new int[2];
+        int[] front1 = new int[2];
+        int[] front2 = new int[2];
         
         // Iterate through the array backwards
         for (int ind = n - 1; ind >= 0; ind--) {
@@ -9,18 +11,22 @@ class Solution {
                 int profit = 0;
 
                 if (buy == 0) { // We can buy the stock
-                    profit = Math.max(0 + dp[ind + 1][0], -prices[ind] + dp[ind + 1][1]);
+                    profit = Math.max(0 + front1[0], -prices[ind] + front1[1]);
                 }
 
                 if (buy == 1) { // We can sell the stock
-                    profit = Math.max(0 + dp[ind + 1][1], prices[ind] + dp[ind + 2][0]);
+                    profit = Math.max(0 + front1[1], prices[ind] + front2[0]);
                 }
 
-                dp[ind][buy] = profit;
+                cur[buy] = profit;
             }
+
+            // Update front1 and front2 arrays
+            System.arraycopy(front1, 0, front2, 0, 2);
+            System.arraycopy(cur, 0, front1, 0, 2);
         }
 
-        // The maximum profit is stored in dp[0][0]
-        return dp[0][0];
+        // The maximum profit is stored in cur[0]
+        return cur[0];
     }
 }
