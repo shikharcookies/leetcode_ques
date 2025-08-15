e1767bf3554a45750c434de9246286c1a0fae759
Shikhar <shikharsingh22803@gmail.com>
2025-08-15 22:48:24 +0530

    Time: 12 ms (92.62%), Space: 52.9 MB (30.05%) - LeetHub

---
+++ 0922-possible-bipartition/0922-possible-bipartition.java
@@ -0,0 +1,45 @@
+class Solution {
+    public boolean possibleBipartition(int n, int[][] dislikes) {
+        List<Integer>[] graph = new List[n+1];
+
+        for(int i=1;i<=n;i++)
+        {
+            graph[i]= new ArrayList<>();
+        }
+
+        for(int[] dislike: dislikes)
+        {
+            graph[dislike[0]].add(dislike[1]);
+            graph[dislike[1]].add(dislike[0]);
+        }
+
+        Integer[] colors = new Integer[n+1];
+
+        for(int i=1;i<=n;i++)
+        {
+            if(colors[i]==null && !dfs(graph,colors,i,0)) return false;
+        }
+        return true;
+    }
+
+    public boolean dfs(List<Integer>[] graph, Integer[]colors, int currNode, int currColor)
+    {
+        colors[currNode]= currColor;
+
+        for(Integer neighborNode : graph[currNode])
+        {
+            if(colors[neighborNode]==null)
+            {
+                if(!dfs(graph,colors,neighborNode,1-currColor))
+                {
+                    return false;
+                }
+            }
+            else if(colors[neighborNode]==currColor)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
